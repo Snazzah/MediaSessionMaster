6c5cc4e3452863a8a0a11f2c5b9a84217252b295
Snazzah <suggesttosnazzy@gmail.com>
2020-11-24 14:37:18 -0600

    fix(twitch): hotfix

--- sites/twitch.js
+++ sites/twitch.js
@@ -1,10 +1,16 @@
 Util.ensureSite('twitch', () => {
   navigator.mediaSession.metadata = new MediaMetadata();
+  let observingAvatar = false;
 
   const updateChannel = () => {
-    const avatar = document.querySelector('.channel-header__user-avatar img');
-    const username = document.querySelector('.channel-header__user-avatar + p');
-    navigator.mediaSession.metadata.title = username.innerText;
+    const avatar = document.querySelector('.channel-root__info .tw-image-avatar');
+    const username = document.querySelector('.channel-root__info .tw-title');
+    if (!username || !avatar) return;
+    if (!observingAvatar) {
+      observingAvatar = true;
+      observer.observe(avatar, { attributes: true });
+    }
+    navigator.mediaSession.metadata.title = username.textContent;
     navigator.mediaSession.metadata.artist = 'Twitch';
     navigator.mediaSession.metadata.artwork = [
       {
@@ -26,14 +32,15 @@ Util.ensureSite('twitch', () => {
   };
 
   const verifyObserver = new MutationObserver(mutations => {
-    const nameMutation = mutations.find(mutation => mutation.attributeName === "data-a-page-loaded-name");
+    const nameMutation = mutations.find(mutation => mutation.attributeName === 'data-a-page-loaded-name');
     if(!nameMutation) return;
     else {
       switch(document.querySelector('#root').getAttribute('data-a-page-loaded-name')) {
+        case 'ChannelWatchPage':
         case 'ChannelPage':
           verifyObserver.disconnect();
           const observer = new MutationObserver(updateChannel);
-          observer.observe(document.querySelector('.channel-header__user-avatar img'), { attributes: true });
+          observer.observe(document.querySelector('.channel-root__info'), { attributes: true, childList: true });
           updateChannel();
           break;
       }
