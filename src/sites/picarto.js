e1f6706377fd340c286786925b9c1958357a18b5
Snazzah <suggesttosnazzy@gmail.com>
2022-02-17 19:10:55 -0600

    fix(picarto): fix picarto metadata

--- src/sites/picarto.js
+++ src/sites/picarto.js
@@ -1,14 +1,49 @@
 Util.ensureSite('picarto', () => {
-  if(!document.querySelector('#userbar-name')) return;
-  navigator.mediaSession.metadata = new MediaMetadata({
-    artist: 'Picarto',
-    title: document.querySelector('#userbar-name .text-white').innerText,
-    artwork: [
-      {
-        src: document.querySelector('#userbar-avatar img').src,
-        sizes: '100x100',
-        type: 'image/jpeg'
-      },
-    ]
-  });
+  let lastPicture = null;
+  const updateInfo = () => {
+    if(!document.querySelector('[class^="styled__ChannelLeftContent"] [class^="ChannelToolbarTitle"], [class^="MinimizedStream"], .mistvideo-curentTime')) {
+      navigator.mediaSession.metadata = null;
+      return;
+    }
+
+    if(document.querySelector('[class^="styled__ChannelLeftContent"] [class^="ChannelToolbarTitle"]')) {
+      lastPicture = document.querySelector('[class^="styled__ChannelLeftContent"] .ant-avatar img').src;
+      navigator.mediaSession.metadata = new MediaMetadata({
+        artist: 'Picarto',
+        album: 'Picarto',
+        title: document.querySelector('[class^="styled__ChannelLeftContent"] [class^="ChannelToolbarTitle"]').textContent,
+        artwork: [
+          {
+            src: lastPicture,
+            sizes: '100x100',
+            type: 'image/jpeg'
+          },
+        ]
+      });
+    } else if (document.querySelector('[class^="MinimizedStream"]')) {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        artist: 'Picarto',
+        album: 'Picarto',
+        title: document.querySelector('[class^="MinimizedStream"] span').textContent,
+        artwork: [
+          {
+            src: lastPicture,
+            sizes: '100x100',
+            type: 'image/jpeg'
+          },
+        ]
+      });
+    }
+  };
+
+  const onMutation = mutations => {
+    mutations.forEach(mutation => {
+      if(!mutation.target) return;
+      updateInfo();
+    });
+  };
+
+  const observer = new MutationObserver(onMutation);
+  observer.observe(document.querySelector('#root'), { subtree: true, attributes: true });
+  updateInfo();
 });
